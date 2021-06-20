76434debe739bde522b65ca01eb23b1afafbcffa
raduOarga <raduoarga95@gmail.com>
2021-06-20 14:23:57 +0200

    Switch to different style
    Hover sets a random color

--- js/script.js
+++ js/script.js
@@ -9,8 +9,6 @@ boardArea.appendChild(pageTitle);
 let board;
 
 let squareNumber = 16;
-
-
 const makeBoard = () => {
     board = document.createElement("div");
     board.setAttribute("id", "board");
@@ -19,18 +17,19 @@ const makeBoard = () => {
         let boardRow = document.createElement("div");
         boardRow.classList.add("boardRow");
         board.appendChild(boardRow);
-        console.log("made row");
         for(let j = 0; j < squareNumber; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
             boardRow.appendChild(square);
-            console.log("made squares");    
         }
     }
     let squares = board.querySelectorAll(".square");
 squares.forEach((square) => {
     square.addEventListener("mouseover", (e) => {
-        square.setAttribute("id", "touched");
+        if(square.id != "touched") {
+            square.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            square.setAttribute("id", "touched");
+        }
     })
 })
 }
@@ -38,7 +37,13 @@ squares.forEach((square) => {
 makeBoard();
 
 restartButton.addEventListener("click", (e) => {
-    squareNumber = parseInt(prompt("How many squares should the next board have on each side? (2 - 100)"));
+    squareNumber = parseInt(prompt("How many squares should the next board have on each side? (100 max)"));
+    console.log(squareNumber);
+    if(squareNumber > 100) {
+        squareNumber = 100;
+    } else if(!(squareNumber > 0 && squareNumber < 100)) {
+        squareNumber = 16;
+    }
     boardArea.removeChild(board);
     makeBoard();
 })
