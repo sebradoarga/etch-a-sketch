d5cea6595d45fecef7d091635da5830f6aec5f29
raduOarga <raduoarga95@gmail.com>
2021-06-20 13:36:53 +0200

    Make square number adjustable

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 const boardArea = document.querySelector("#board-area");
+const restartButton = document.querySelector("button");
 
 const pageTitle = document.createElement("h1");
 pageTitle.textContent = "Etch-a-Sketch";
 pageTitle.setAttribute("id", "pageTitle");
 boardArea.appendChild(pageTitle);
 
-const board = document.createElement("div");
-board.setAttribute("id", "board");
-boardArea.appendChild(board);
+let board;
+
+let squareNumber = 16;
 
 
 const makeBoard = () => {
-    for(let i = 0; i < 16; i++) {
+    board = document.createElement("div");
+    board.setAttribute("id", "board");
+    boardArea.appendChild(board);
+    for(let i = 0; i < squareNumber; i++) {
         let boardRow = document.createElement("div");
         boardRow.classList.add("boardRow");
         board.appendChild(boardRow);
-        for(let j = 0; j < 16; j++) {
+        console.log("made row");
+        for(let j = 0; j < squareNumber; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
-            boardRow.appendChild(square);    
+            boardRow.appendChild(square);
+            console.log("made squares");    
         }
     }
-}
-
-makeBoard();
-
-let squares = board.querySelectorAll(".square");
-console.log(squares);
+    let squares = board.querySelectorAll(".square");
 squares.forEach((square) => {
     square.addEventListener("mouseover", (e) => {
         square.setAttribute("id", "touched");
     })
+})
+}
+
+makeBoard();
+
+restartButton.addEventListener("click", (e) => {
+    squareNumber = parseInt(prompt("How many squares should the next board have on each side? (2 - 100)"));
+    boardArea.removeChild(board);
+    makeBoard();
 })
